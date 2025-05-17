f7a8306bb2546623746ca84e58030eed8a4ba818
Melody Bechler <melodybechler@alumni.emory.edu>
2025-05-16 20:46:26 -0500

    added .env and azure forward translation

--- googlecloud/google_cloud_translation.py
+++ googlecloud/google_cloud_translation.py
@@ -1,5 +1,6 @@
 from google.cloud import translate_v2 as translate
 import os
+from dotenv import load_dotenv
 
 #translates text using Neural Machine Translation in Google Cloud Translation API
 
@@ -13,34 +14,35 @@ figured it would be easiest to fix all of this once we have the code to read the
 '''
   
 def load_alerts(data_dir):
-#load txt files and their contents from data directory and store as list	
+#load txt files and their contents from data directory and store as list
 	alerts = []
 	for root, _, files in os.walk(data_dir):
-        	for file in files:
-            		if file.endswith(".txt"):
-                		full_path = os.path.join(root, file)
-                		with open(full_path, "r", encoding="utf-8") as f:
-                    			content = f.read()
-                		alerts.append((file, content))
-    	return alerts
+		for file in files:
+			if file.endswith(".txt"):
+				full_path = os.path.join(root, file)
+				with open(full_path, "r", encoding="utf-8") as f:
+					content = f.read()
+				alerts.append((file, content))
+	return alerts
 	
 def translate_text(alerts, lang):
-	#initialize translation client
-	translate_client=translate.Client()
+	#initialize translation client, key should be in .env file
+	translate_client=translate.Client(key=os.getenv("GOOGLE_APPLICATION_CREDENTIALS")) #need to add key and hide .env file
 
 	#new loop to read through alerts dict and translate each file
-	for key, value in alerts.items():
+	for k, value in alerts.items():
 		#create new file for each translated file, using category key and lang code
-		with open(f"{key}_{lang}.txt", "w") as outFile:    
+		with open(f"{k}_{lang}.txt", "w") as outFile:    
 			translation=translate_client.translate(value, target_language=lang, source_language="en") #should shut off auto lang detection that eats credits
 			outFile.write(translation['translatedText'])
 	outFile.close()
-
+	
 def main():
 	#read in data files from data directory, probably should store the name of the file in var too
 	#also need to read in api key from env variable
-	os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "path/to/your/credentials.json"
-	
+	#os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "path/to/your/credentials.json"
+	load_dotenv() #reads in env variables from .env file
+
 	#below needs to be edited to correctly read in the files
 	data_dir = "data" #hardcode actual path to data directory?
 
