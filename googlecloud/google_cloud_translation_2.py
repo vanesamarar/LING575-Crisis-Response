42604b3197604ce8fe42728a2651468506f36174
vanesamarar <vanesamarar@yahoo.com>
2025-05-22 20:11:25 -0700

    Update google_cloud_translation_2.py

--- googlecloud/google_cloud_translation_2.py
+++ googlecloud/google_cloud_translation_2.py
@@ -1,12 +1,16 @@
-from google.cloud import translate_v2 as translate
+#from google.cloud import translate_v2 as translate
+from unittest.mock import MagicMock
 import os
 
+class MockTranslateClient:
+	def translate(self, text, target_language, source_language="en"):
+		return {'translatedText': f"[{target_language} translation of]: {text}"}
+
 translation_langs = ["es", "vi", "ko", "km", "so"]
-input_dir = "data"
-output_dir = "translations"
+input_dir = "../data"
+output_dir = "../translations"
 
 def load_alerts(data_dir):
-#load txt files and their contents from data directory and store as list
   	alerts = []
   	for root, _, files in os.walk(data_dir):
 		for file in files:
@@ -18,24 +22,20 @@ def load_alerts(data_dir):
 	return alerts
 
 def translate_text(alerts, lang, out_dir):
-	translate_client = translate.Client()
+	#translate_client = translate.Client()
+	translate_client = MockTranslateClient()
 	lang_dir = os.path.join(out_dir, lang)
 	os.makedirs(lang_dir, exist_ok=True)
 
 	for file, content in alerts:
-		translation = translate_client.translate(
-            	content,
-            	target_language=lang,
-            	source_language="en"
-        	)
+		translation = translate_client.translate(content, target_language=lang, source_language="en")
         	out_path = os.path.join(lang_dir, file)
         	with open(out_path, "w", encoding="utf-8") as f:
             		f.write(translation['translatedText'])
         	print(f"Translated {file} to {lang}")
 
 def main():
-	#os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/vanesamarar/Downloads/crisis-alerts-translation-873a62787e85.json"
-	os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+	#os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
 	alerts = load_alerts(input_dir)
 
 	for lang in translation_langs:
