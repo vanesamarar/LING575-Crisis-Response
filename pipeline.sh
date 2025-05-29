0733fd6db4a7d9d217d2a8967cd31bb1fcd6526d
vanesamarar <vanesamarar@yahoo.com>
2025-05-29 10:55:48 -0700

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
 
+echo "Starting pipeline..."
+
+echo "Running Azure forward translation..."
+python3 azure/forward_translate.py
+
+echo "Running Google forward translation..."
+python3 googlecloud/forward_translate.py 
+
+echo "Running forward evaluation..."
+python3 evaluation/forward_evaluation.py
