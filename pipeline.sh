3b615ecdc84bd486cdf87959d280c76e63c14f47
vanesamarar <vanesamarar@yahoo.com>
2025-05-29 15:28:24 -0700

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -1,2 +1,25 @@
-#!/bin/sh
+#!/bin/bash
+
+set -e
+echo "Starting pipeline..."
+
+echo "Running Azure forward translation..."
+python3 azure/forward_translate.py
+
+echo "Running Google forward translation..."
+python3 googlecloud/forward_translate.py 
+
+echo "Running forward evaluation..."
+python3 evaluation/forward_evaluation.py
+
+echo "Running Azure backward translation..."
+python3 azure/backward_translate.py
+
+echo "Running Google backward translation..."
+python3 googlecloud/backward_translate.py
+
+echo "Running backward evaluation..."
+python3 evaluation/backward_evaluation.py
+
+echo "Pipeline ran successfully."
 
