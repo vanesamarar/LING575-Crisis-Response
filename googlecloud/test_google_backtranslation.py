053eeb97b18060b3aa216005f241c80794f72085
vanesamarar <vanesamarar@yahoo.com>
2025-05-30 14:07:12 -0700

    Update test_google_backtranslation.py

--- googlecloud/test_google_backtranslation.py
+++ googlecloud/test_google_backtranslation.py
@@ -4,11 +4,10 @@ from google.cloud import translate_v2 as translate
 
 
 #adjust load_alerts to account for directory formatting from forward translation -- see notes in main()
-def load_alerts(data_dir):
-#load txt files and their contents from data directory and store as list
+def load_alerts(input_dir):
 	alerts = []
-	for root, _, files in os.walk(data_dir):
-		for file in files:
+	for root, _, files in os.walk(input_dir):
+		for file in sorted(files):
 			if file.endswith(".txt"):
 				full_path = os.path.join(root, file)
 				with open(full_path, "r", encoding="utf-8") as f:
@@ -20,41 +19,31 @@ def translate_text(alerts, lang):
 	#initialize translation client, key should be in .env file -- utilizes second google API key
 	translate_client=translate.Client(key=os.getenv("GOOGLE_APPLICATION_CREDENTIALS_2")) #need to add key and hide 
 
+	output_dir = os.path.join("googlecloud", "back_translations", lang)
+	os.makedirs(output_dir, exist_ok=True)
 	#new loop to read through alerts dict and translate each file
 	for k, value in alerts.items():
 		#create new file for each translated file, using category key and lang code
-		with open(f"{k}_{lang}_en.txt", "w") as outFile:    
+		'''with open(f"{k}_{lang}_en.txt", "w") as outFile:    
 			translation=translate_client.translate(value, target_language="en", source_language=lang)
 			outFile.write(translation['translatedText'])
-	outFile.close()
+	outFile.close()'''
+		translated = translate_client.translate(value, target_language="en", source_language=lang)
+        	output_path = os.path.join(output_dir, k)
+        	with open(output_path, "w", encoding="utf-8") as outFile:
+            		outFile.write(translated["translatedText"])
+
 
 
 #main
 def main():
 	load_dotenv() #reads in env variables from .env file
 
-	#for backtranslation we probably should read each directory and their associated files separately for each language
-	data_dir = "data" #hardcode actual path to data directory?
-	
-    #translate back to English from Spanish; alerts should overwrite/delete previous loaded translations for each load_alerts call
-	alerts = load_alerts(data_dir)
-	translate_text(alerts, "es")
-	
-    #translate back to English from Vietnamese
-	alerts = load_alerts(data_dir)
-	translate_text(alerts, "vi")
-	
-    #translate back to English from Korean
-	alerts=load_alerts(data_dir)
-	translate_text(alerts, "ko")
-	
-    #translate back to English from Khmer
-	alerts=load_alerts(data_dir)
-	translate_text(alerts, "km")
-	
-    #translate back to English from Somali
-	alerts=load_alerts(data_dir)
-	translate_text(alerts, "so")
+	langs = ["es", "vi", "ko", "km", "so"]
+    	for lang in langs:
+        	input_dir = os.path.join("googlecloud", "forward_translations", lang)
+        	alerts = load_alerts(input_dir)
+        	translate_text(alerts, lang)
 
 
 if __name__ == "__main__":
