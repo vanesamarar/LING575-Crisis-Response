050115e068940671e919ff3cd932422fbba70787
vanesamarar <vanesamarar@yahoo.com>
2025-05-30 14:37:39 -0700

    Update test_azure_backtranslation.py

--- azure/test_azure_backtranslation.py
+++ azure/test_azure_backtranslation.py
@@ -2,10 +2,6 @@ import os
 from dotenv import load_dotenv
 import requests, uuid, json
 
-
-#add key and endpoint -- put key and endpoint in .env file
-#key = my api key is listed in my azure portal
- #not sure this is correct..... azure doesnt list my endpoints
 def load_alerts(input_dir):
 	alerts = []
 	for root, _, files in os.walk(input_dir):
@@ -18,41 +14,27 @@ def load_alerts(input_dir):
 	return alerts
 
 def translate_text(alerts, lang):
-	#initialize translation client - utilizes second azure API key
 	key=os.getenv("AZURE_KEY_2") #need to add key and hide .env file
 	endpoint=os.getenv("AZURE_ENDPOINT") #need to add endpoint url to .env ---- I THINK this can be the same endpoint url as forward translation
-
+	region = os.getenv("AZURE_REGION", "westus2")
 
 	path='/translate'
 	constructed_url=endpoint +path
 	params={
 		'api-version':'3.0',
 		'from':lang,
-		'to':"en"
+		'to':'en'
 	}
 	headers={
 		'Content-type':'application/json',
 		'X-ClientTraceId':str(uuid.uuid4()),
 		'Ocp-Apim-Subscription-Key':key,
-		'Ocp-Apim-Subscription-Region':'westus2' #need to check this, otherwise optional
+		'Ocp-Apim-Subscription-Region': region #need to check this, otherwise optional
 	}
 
-	output_dir = os.path.join("azure", "back_translations", lang)
+	output_dir = os.path.join("test_back_translations", lang)
     	os.makedirs(output_dir, exist_ok=True)
 
-	'''for k, value in alerts.items():
-		#create new file for each translated file, using category key and lang c 
-		with open(f"{k}_{lang}_en_.txt", "w") as outFile: 
-			body =[{'text': value}]
-			#make request to azure translation service   
-			request = requests.post(constructed_url, params=params, headers=headers, json=body)
-			response=request.json()
-			#print(json.dumps(response, sort_keys=True, ensure_ascii=False, indent=4, separators=(',', ':')))
-
-			#parse response
-			#response as a list of dictionaries
-			outFile.write(response[0]['translations'][0]['text'])
-		outFile.close()'''
 	for fname, content in alerts:
         	body = [{'text': content}]
         	response = requests.post(constructed_url, params=params, headers=headers, json=body)
@@ -68,7 +50,7 @@ def main():
 	langs = ["es", "vi", "ko", "km", "so"]
 
 	for lang in langs:
-        	input_dir = os.path.join("azure", "forward_translations", lang)
+        	input_dir = os.path.join("test_forward_translations", lang)
         	alerts = load_alerts(input_dir)
         	translate_text(alerts, lang)
 	
