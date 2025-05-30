43d7c9e006ff62f34b6cdd598ef47cfd0db5b7db
vanesamarar <vanesamarar@yahoo.com>
2025-05-30 13:53:34 -0700

    Update and rename pipeline.sh to test_pipeline.sh

--- test_pipeline.sh
+++ test_pipeline.sh
@@ -4,21 +4,21 @@ set -e
 echo "Starting pipeline..."
 
 echo "Running Azure forward translation..."
-python3 azure/forward_translate.py
+python3 azure/test_forward_translate.py
 
 echo "Running Google forward translation..."
-python3 googlecloud/forward_translate.py 
+python3 googlecloud/test_forward_translate.py 
 
 echo "Running forward evaluation..."
-python3 evaluation/forward_evaluation.py
+python3 evaluation/test_forward_evaluation.py
 
 echo "Running Azure backward translation..."
-python3 azure/backward_translate.py
+python3 azure/test_backward_translate.py
 
 echo "Running Google backward translation..."
-python3 googlecloud/backward_translate.py
+python3 googlecloud/test_backward_translate.py
 
 echo "Running backward evaluation..."
-python3 evaluation/backward_evaluation.py
+python3 evaluation/test_backward_evaluation.py
 
 echo "Pipeline ran successfully."
