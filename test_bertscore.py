fe478bd2abc22676853dc63df8667dd39fce62f4
vanesamarar <vanesamarar@yahoo.com>
2025-06-08 12:14:12 -0700

    Delete test_bertscore.py

--- test_bertscore.py
+++
@@ -1,6 +0,0 @@
-from bert_score import score as bert_score
-bts = ["This is a back-translated alert message."]
-refs = ["This is the original English alert message."]
-P, R, F1 = bert_score(bts, refs, lang="en", rescale_with_baseline=True)
-print(F1)  # Should be positive and reasonable
-
